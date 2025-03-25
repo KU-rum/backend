148e7c67eabc78491b4e20c6f4f0562d032a689b
Hyeri1ee <haerizian10@gmail.com>
2025-03-26 02:55:09 +0900

    feat: develop브랜치 머지(#195)

--- src/main/java/ku_rum/backend/BackendApplication.java
+++ src/main/java/ku_rum/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableBatchProcessing
 @EnableAsync
 public class BackendApplication {
 
