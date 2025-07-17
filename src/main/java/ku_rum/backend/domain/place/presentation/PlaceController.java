f4daa2f49234780f9b50bd7bf67ebc3820a55ba5
david-parkk <ji0513ji@naver.com>
2025-07-17 14:38:32 +0900

    feat: 장소 검색 기록 API

--- src/main/java/ku_rum/backend/domain/place/presentation/PlaceController.java
+++ src/main/java/ku_rum/backend/domain/place/presentation/PlaceController.java
@@ -8,6 +8,7 @@ import ku_rum.backend.domain.place.application.response.CurrentPositionConfirmRe
 import ku_rum.backend.domain.place.application.response.CurrentPositionResponse;
 import ku_rum.backend.domain.place.application.response.CurrentPositionStatusResponse;
 import ku_rum.backend.domain.place.application.response.GetPlaceResponse;
+import ku_rum.backend.domain.place.application.response.SearchPlaceHistoryResponse;
 import ku_rum.backend.domain.place.application.response.SearchPlaceResponse;
 import ku_rum.backend.domain.place.application.response.SelectPlaceChipResponse;
 import ku_rum.backend.domain.place.domain.CategoryChip;
@@ -92,6 +93,12 @@ public class PlaceController {
     public BaseResponse<List<SearchPlaceResponse>> searchPlace(@RequestParam("query") String query,
                                                                @AuthenticationPrincipal final CustomUserDetails userDetails) {
         return BaseResponse.ok(placeService.searchPlace(query));
+    }
+
+    @GetMapping("/search/history")
+    public BaseResponse<List<SearchPlaceHistoryResponse>> searchPlaceHistory(
+            @AuthenticationPrincipal final CustomUserDetails userDetails) {
+        return BaseResponse.ok(placeService.searchPlaceHistory(userDetails));
     }
 
     private boolean isAuthenticated(final CustomUserDetails userDetails) {
