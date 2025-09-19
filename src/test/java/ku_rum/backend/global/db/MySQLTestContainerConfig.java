58aadcae7ecc546b40910840295f25e3aa3c548d
david-parkk <ji0513ji@naver.com>
2025-09-20 00:17:31 +0900

    feat: 테스트 컨테이너 추가

---
+++ src/test/java/ku_rum/backend/global/db/MySQLTestContainerConfig.java
@@ -0,0 +1,27 @@
+package ku_rum.backend.global.db;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@TestConfiguration
+@Testcontainers
+public class MySQLTestContainerConfig {
+
+    @Container
+    public static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    public static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysqlContainer::getDriverClassName);
+    }
+}
