bbabc9454db36adfdb93e5c8a58a1cab95d95b99
david-parkk <ji0513ji@naver.com>
2025-10-29 15:29:45 +0900

    feat: 북마크 엔티티 생성

---
+++ src/main/java/ku_rum/backend/domain/bookmark/domain/NoticeBookMark.java
@@ -0,0 +1,32 @@
+package ku_rum.backend.domain.bookmark.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import ku_rum.backend.domain.notice.domain.Notice;
+import ku_rum.backend.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class NoticeBookMark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Notice notice;
+}
