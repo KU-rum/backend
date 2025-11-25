a3273d125b0bdf3ef481da503d730639115ab983
david-parkk <ji0513ji@naver.com>
2025-11-25 16:04:12 +0900

    feat: 친구 신청 알림 추가

--- src/main/java/ku_rum/backend/domain/alarm/application/FriendRequestHandler.java
+++ src/main/java/ku_rum/backend/domain/alarm/application/FriendRequestHandler.java
@@ -1,11 +1,14 @@
 package ku_rum.backend.domain.alarm.application;
 
+import ku_rum.backend.domain.user.domain.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FriendRequestHandler implements AlarmMessageHandler {
     @Override
     public String create(Object payload) {
-        return "";
+        User friend = (User) payload;
+
+        return String.format("%s 님이 친구 신청을 했어요. 친구 신청을 수락하시겠어요?", friend.getNickname());
     }
 }
