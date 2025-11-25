e1a967aa4bd98c010a1f0973984e586f880c84e6
david-parkk <ji0513ji@naver.com>
2025-11-25 17:22:41 +0900

    refactor: 클래스 분리

---
+++ src/main/java/ku_rum/backend/domain/notice/presentation/KeywordController.java
@@ -0,0 +1,33 @@
+package ku_rum.backend.domain.notice.presentation;
+
+import ku_rum.backend.domain.notice.application.SearchKeywordService;
+import ku_rum.backend.domain.notice.dto.request.SaveKeywordRequest;
+import ku_rum.backend.domain.notice.dto.response.GetKeywordResponse;
+import ku_rum.backend.global.security.CustomUserDetails;
+import ku_rum.backend.global.support.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/notices")
+@RequiredArgsConstructor
+public class KeywordController {
+    private final SearchKeywordService searchKeywordService;
+
+    @GetMapping("/keyword")
+    public BaseResponse<GetKeywordResponse> getKeywords(
+            @AuthenticationPrincipal final CustomUserDetails userDetails) {
+        return BaseResponse.ok(searchKeywordService.getKeyword(userDetails));
+    }
+
+    @PostMapping("/keyword")
+    public BaseResponse<Void> saveKeyword(
+            @AuthenticationPrincipal final CustomUserDetails userDetails,
+            @RequestBody SaveKeywordRequest request) {
+        searchKeywordService.save(request, userDetails);
+        return BaseResponse.ok();
+    }
+}
--- src/main/java/ku_rum/backend/domain/notice/presentation/NoticeController.java
+++ src/main/java/ku_rum/backend/domain/notice/presentation/NoticeController.java
@@ -2,21 +2,14 @@ package ku_rum.backend.domain.notice.presentation;
 
 import java.util.List;
 import ku_rum.backend.domain.notice.application.NoticeService;
-import ku_rum.backend.domain.notice.application.SearchKeywordService;
-import ku_rum.backend.domain.notice.dto.request.SaveKeywordRequest;
-import ku_rum.backend.domain.notice.dto.response.GetKeywordResponse;
 import ku_rum.backend.domain.notice.dto.response.NoticeDetailResponse;
 import ku_rum.backend.domain.notice.dto.response.NoticeResponse;
-import ku_rum.backend.global.security.CustomUserDetails;
 import ku_rum.backend.global.support.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NoticeController {
     private final NoticeService noticeService;
-    private final SearchKeywordService searchKeywordService;
 
     @GetMapping
     public Page<NoticeResponse> getNoticesByCategory(
@@ -60,19 +52,5 @@ public class NoticeController {
             Pageable pageable
     ) {
         return noticeService.searchByKeyword(keyword, pageable);
-    }
-
-    @GetMapping("/keyword")
-    public BaseResponse<GetKeywordResponse> getKeywords(
-            @AuthenticationPrincipal final CustomUserDetails userDetails) {
-        return BaseResponse.ok(searchKeywordService.getKeyword(userDetails));
-    }
-
-    @PostMapping("/keyword")
-    public BaseResponse<Void> saveKeyword(
-            @AuthenticationPrincipal final CustomUserDetails userDetails,
-            @RequestBody SaveKeywordRequest request) {
-        searchKeywordService.save(request, userDetails);
-        return BaseResponse.ok();
     }
 }
