3cada85be4b5e44fd44cd38fa85e277f24e3d7a0
david-parkk <ji0513ji@naver.com>
2025-11-30 17:40:14 +0900

    fix: 알림 카테고리 추가

--- src/main/java/ku_rum/backend/domain/alarm/dto/response/GetAlarmDto.java
+++ src/main/java/ku_rum/backend/domain/alarm/dto/response/GetAlarmDto.java
@@ -2,19 +2,22 @@ package ku_rum.backend.domain.alarm.dto.response;
 
 import java.time.LocalDateTime;
 import ku_rum.backend.domain.alarm.domain.Alarm;
+import ku_rum.backend.domain.alarm.domain.AlarmCategory;
 import ku_rum.backend.domain.alarm.domain.AlarmType;
 import ku_rum.backend.domain.alarm.domain.Announcement;
 import ku_rum.backend.domain.alarm.domain.UserAnnouncement;
 import lombok.Builder;
 
 @Builder
-public record GetAlarmDto(Long id, AlarmType alarmType, String message, boolean isChecked, String dataId,
+public record GetAlarmDto(Long id, AlarmType alarmType, AlarmCategory alarmCategory, String message, boolean isChecked,
+                          String dataId,
                           LocalDateTime createdAt) {
 
     public static GetAlarmDto from(Alarm alarm) {
         return GetAlarmDto.builder()
                 .id(alarm.getId())
                 .alarmType(alarm.getAlarmType())
+                .alarmCategory(alarm.getAlarmType().getAlarmCategory())
                 .message(alarm.getMessage())
                 .isChecked(alarm.isChecked())
                 .dataId(alarm.getDataId())
@@ -27,6 +30,7 @@ public record GetAlarmDto(Long id, AlarmType alarmType, String message, boolean 
         return GetAlarmDto.builder()
                 .id(announcement.getId())
                 .alarmType(announcement.getAlarmType())
+                .alarmCategory(userAnnouncement.getAnnouncement().getAlarmType().getAlarmCategory())
                 .message(announcement.getMessage())
                 .dataId(userAnnouncement.getDataId())
                 .createdAt(userAnnouncement.getCreatedAt())
