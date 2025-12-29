c0a90c909c2a0790d2f77334f29d3bc8d1d4f347
david-parkk <ji0513ji@naver.com>
2025-12-29 15:57:37 +0900

    fix: 알림 설정 API 수정
    - 배열로 변경

--- src/main/java/ku_rum/backend/domain/alarm/dto/response/PatchDisableAlarmResponse.java
+++ src/main/java/ku_rum/backend/domain/alarm/dto/response/PatchDisableAlarmResponse.java
@@ -1,11 +1,30 @@
 package ku_rum.backend.domain.alarm.dto.response;
 
-import ku_rum.backend.domain.alarm.domain.AlarmType;
+import java.util.ArrayList;
+import java.util.List;
 import ku_rum.backend.domain.alarm.domain.UserDisabledAlarm;
 
-public record PatchDisableAlarmResponse(Long userId, AlarmType alarmType, Boolean isDisabled) {
-    public static PatchDisableAlarmResponse of(UserDisabledAlarm userDisabledAlarm, boolean isDisabled) {
-        return new PatchDisableAlarmResponse(userDisabledAlarm.getUser().getId(), userDisabledAlarm.getAlarmType(),
-                isDisabled);
+public record PatchDisableAlarmResponse(Long userId, List<DisableAlarmDto> alarms) {
+    public static PatchDisableAlarmResponse of(List<UserDisabledAlarm> toCreate, List<UserDisabledAlarm> toDelete) {
+        Long userId = null;
+        if (!toCreate.isEmpty()) {
+            userId = toCreate.get(0).getUser().getId();
+        } else if (!toDelete.isEmpty()) {
+            userId = toDelete.get(0).getUser().getId();
+        }
+
+        List<DisableAlarmDto> createdAlarms = toCreate.stream()
+                .map(userDisabledAlarm -> DisableAlarmDto.of(userDisabledAlarm, false))
+                .toList();
+
+        List<DisableAlarmDto> deletedAlarms = toDelete.stream()
+                .map(userDisabledAlarm -> DisableAlarmDto.of(userDisabledAlarm, true))
+                .toList();
+
+        List<DisableAlarmDto> allAlarms = new ArrayList<>();
+        allAlarms.addAll(createdAlarms);
+        allAlarms.addAll(deletedAlarms);
+
+        return new PatchDisableAlarmResponse(userId, allAlarms);
     }
 }
