1d560a0150253364d8897f1886abfa6e69134986
Park Jiwon <57484954+david-parkk@users.noreply.github.com>
2026-01-01 16:52:35 +0900

    [Fix] 친구 추가시 알림 버그 수정 (#378)
    - 친구 생성시 올바른 알림 생성

--- src/main/java/ku_rum/backend/domain/friend/application/FriendManageService.java
+++ src/main/java/ku_rum/backend/domain/friend/application/FriendManageService.java
@@ -41,7 +41,7 @@ public class FriendManageService {
         }
         friendRepository.save(Friend.of(fromUser, toUser, PENDING));
 
-        alarmService.notifyAlarm(AlarmType.NEW_FRIEND_REQUEST, toUser);
+        alarmService.notifyAlarm(AlarmType.NEW_FRIEND_REQUEST, fromUser, toUser);
     }
 
     // 친구 수락 및 거절
