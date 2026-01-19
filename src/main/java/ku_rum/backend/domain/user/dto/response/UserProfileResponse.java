50d54c9242418f8f08e1f0dde7d9df667479f0b7
david-parkk <ji0513ji@naver.com>
2026-01-19 18:03:48 +0900

    fix: 프로필 이미지, 로그인 아이디 추가

--- src/main/java/ku_rum/backend/domain/user/dto/response/UserProfileResponse.java
+++ src/main/java/ku_rum/backend/domain/user/dto/response/UserProfileResponse.java
@@ -2,6 +2,6 @@ package ku_rum.backend.domain.user.dto.response;
 
 import java.util.List;
 
-public record UserProfileResponse(String email, String nickname, String studentId,
+public record UserProfileResponse(String profileImage, String email, String loginId, String nickname, String studentId,
                                   List<UserProfileDepartmentResponse> departments) {
 }
