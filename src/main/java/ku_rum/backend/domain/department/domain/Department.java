69490a87eee8f176dac7d3efce7fd998c1c54d9d
david-parkk <ji0513ji@naver.com>
2026-01-22 16:05:34 +0900

    feat: 학과 url 조회 API

--- src/main/java/ku_rum/backend/domain/department/domain/Department.java
+++ src/main/java/ku_rum/backend/domain/department/domain/Department.java
@@ -1,9 +1,20 @@
 package ku_rum.backend.domain.department.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import ku_rum.backend.domain.college.domain.College;
 import ku_rum.backend.global.support.type.BaseEntity;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Entity
@@ -18,21 +29,24 @@ public class Department extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "college_id")
     private College college;
 
+    private String url;
+
     @Builder
-    private Department(String name, College college) {
+    private Department(String name, College college, String url) {
         this.name = name;
         this.college = college;
+        this.url = url;
     }
 
-    public static Department of(String name, College college) {
+    public static Department of(String name, College college, String url) {
         return Department.builder()
                 .name(name)
                 .college(college)
+                .url(url)
                 .build();
     }
 }
---
+++ src/main/java/ku_rum/backend/domain/department/dto/DepartmentUrlResponse.java
@@ -0,0 +1,4 @@
+package ku_rum.backend.domain.department.dto;
+
+public record DepartmentUrlResponse(String name, String url) {
+}
