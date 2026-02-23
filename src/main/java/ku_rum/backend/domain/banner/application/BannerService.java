6f3de5c639dc3cf142a33516993eb79623d3b93e
david-parkk <ji0513ji@naver.com>
2026-02-23 20:12:34 +0900

    fix: 리뷰 반영
    - validation 추가

--- src/main/java/ku_rum/backend/domain/banner/application/BannerService.java
+++ src/main/java/ku_rum/backend/domain/banner/application/BannerService.java
@@ -2,6 +2,7 @@ package ku_rum.backend.domain.banner.application;
 
 import static ku_rum.backend.global.support.status.BaseExceptionResponseStatus.URL_NOT_BANNER;
 
+import java.util.ArrayList;
 import java.util.List;
 import ku_rum.backend.domain.banner.domain.Banner;
 import ku_rum.backend.domain.banner.domain.repository.BannerRepository;
@@ -10,12 +11,13 @@ import ku_rum.backend.domain.banner.dto.PatchBannerRequests;
 import ku_rum.backend.domain.common.image.application.S3ImageService;
 import ku_rum.backend.global.exception.global.GlobalException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-
+@Slf4j
 public class BannerService {
 
     private final BannerRepository bannerRepository;
@@ -31,16 +33,24 @@ public class BannerService {
 
     @Transactional
     public void addBanner(PatchBannerRequests requests) {
-        List<Banner> banners = requests.banners().stream()
-                .map(request -> {
-                    String imageUrl = s3ImageService.uploadBannerImage(request.images());
-                    return Banner.builder()
-                            .imageUrl(imageUrl)
-                            .link(request.link())
-                            .build();
-                })
-                .toList();
-        bannerRepository.saveAll(banners);
+        List<String> uploadedUrls = new ArrayList<>();
+        try {
+            List<Banner> banners = requests.banners().stream()
+                    .map(request -> {
+                        String imageUrl = s3ImageService.uploadBannerImage(request.images());
+                        uploadedUrls.add(imageUrl);
+                        return Banner.builder()
+                                .imageUrl(imageUrl)
+                                .link(request.link())
+                                .build();
+                    })
+                    .toList();
+            bannerRepository.saveAll(banners);
+            throw new RuntimeException();
+        } catch (Exception e) {
+            log.error("배너 등록 중 오류 발생. 업로드된 파일 롤백. uploadedUrls={}", uploadedUrls, e);
+            s3ImageService.deleteBannerImages(uploadedUrls);
+        }
     }
 
     @Transactional
